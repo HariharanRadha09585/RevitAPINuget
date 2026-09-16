24dfa6235cd6cbe397cb4dff11a9417a844c385e
Hariharan Radhakrishnan <hariharanradha098@gmail.com>
2026-09-16 19:24:56 +0530

    Add project files.

---
+++ RevitAPINuget/Elements/ElementHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace RevitAPINuget.Elements
+{
+    public class ElementHelper
+    {
+        public static string GetElementName(Autodesk.Revit.DB.Element element)
+        {
+            if (element == null)
+            {
+                return "Element is null";
+            }
+            string name = element.Name;
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Element has no name";
+            }
+            return name;
+        }
+    }
+}
---
+++ RevitAPINuget/Views/ViewHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RevitAPINuget.Views
+{
+    public static class ViewHelper
+    {
+        public static string GetViewName(Autodesk.Revit.DB.View view)
+        {
+            if (view == null)
+            {
+                return "View is null";
+            }
+            string name = view.Name;
+            if (string.IsNullOrEmpty(name))
+            {
+                return "View has no name";
+            }
+            return name;
+        }
+    }
+}
